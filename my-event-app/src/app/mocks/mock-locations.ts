91868fb028b6229fcf1357d06b30ea61f0df432c
GeSu07 <gerald.suessmaier@edu.campus02.at>
2019-01-22 22:15:02 +0100

    mock location

--- my-event-app/src/app/mocks/mock-locations.ts
+++ my-event-app/src/app/mocks/mock-locations.ts
@@ -1,6 +1,11 @@
 import { Location } from '../types/location';
 
 export const LOCATION: Location[] = [
-  { id: 1, name: 'Grazer Messe', description: 'sth about Grazer Messe', address: 'Messeplatz 1', zipcode: '8010', countryAbb: 'AUT'}
-
+  { id: 1, name: 'Grazer Messe', description: 'sth about Grazer Messe', address: 'Messeplatz 1', zipcode: '8010', location: 'Graz', countryAbb: 'AUT'},
+  { id: 2, name: 'Wiener Stadthalle', description: 'sth about Wiener Stadthalle', address: 'Roland-Rainer-Platz 1', zipcode: '1150', location: 'Wien', countryAbb: 'AUT'},
+  { id: 3, name: 'Pannonia Fields', description: 'sth about Pannonia Fields', address: 'Grenzlandhof', zipcode: '2425', location: 'Nickelsdorf', countryAbb: 'AUT'},
+  { id: 4, name: 'Green Park', description: 'sth about Green Park', address: 'Kelsengasse 9', zipcode: '3100', location: 'St. Pölten', countryAbb: 'AUT'},
+  { id: 5, name: 'Reifnitz Freigelände', description: 'sth about Reifnitz', address: 'Wörthersee Süduferstraße 115', zipcode: '9081', location: 'Reifnitz', countryAbb: 'AUT' },
+  { id: 6, name: 'Schwarzlsee', description: 'sth. about Schwarzlsee', address: 'Thalerhofstraße 85', zipcode: '8141', location: 'Premstätten', countryAbb: 'AUT'},
+  { id: 7, name: 'Nürburgring/Eifle', description: 'sth about Nürburgring', address: 'Otto-Flimm-Straße', zipcode: '53520', location: 'Nürburgring/Eifel', countryAbb: 'BRD'}
 ];
