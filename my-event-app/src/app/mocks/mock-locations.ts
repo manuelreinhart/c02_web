1855a01c8559ddc6b1c79a3d7ea9853c920365fe
GeSu07 <gerald.suessmaier@edu.campus02.at>
2019-01-22 21:42:06 +0100

    mock location

---
+++ my-event-app/src/app/mocks/mock-locations.ts
@@ -0,0 +1,6 @@
+import { Location } from '../types/location';
+
+export const LOCATION: Location[] = [
+  { id: 1, name: 'Grazer Messe', description: 'sth about Grazer Messe', address: 'Messeplatz 1', zipcode: '8010', countryAbb: 'AUT'}
+
+];
