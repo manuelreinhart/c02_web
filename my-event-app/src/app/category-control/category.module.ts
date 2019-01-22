662ce640378dec2b0512470890523dcd27bd06f1
Roman Zotter <roman.zotter@edu.campus02.at>
2019-01-22 19:15:00 +0100

    Renamed Kategorie to Genre and added Category-module

---
+++ my-event-app/src/app/category-control/category.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatDialogModule } from '@angular/material';
+import { CategoryControlListComponent } from './category-control-list/category-control-list.component';
+import { CategoryControlEditComponent } from './category-control-edit/category-control-edit.component';
+import { CategorySearchComponent } from './category-search/category-search.component';
+import { CategoryService } from '../services/category.service';
+
+@NgModule({
+  declarations: [
+      CategoryControlListComponent, 
+      CategoryControlEditComponent, 
+      CategorySearchComponent],
+  exports: [
+      CategoryControlListComponent, 
+      CategoryControlEditComponent, 
+      CategorySearchComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatDialogModule
+
+  ],
+  providers: [CategoryService],
+  entryComponents: [CategorySearchComponent, CategoryControlEditComponent]  
+})
+export class CategoryModule { }
