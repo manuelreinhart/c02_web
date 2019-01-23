70be341e22d2ab92e115aef3d874e29cb65e5c48
GeSu07 <gerald.suessmaier@edu.campus02.at>
2019-01-24 00:07:21 +0100

    location css angepasst

--- my-event-app/src/app/mocks/mock-locations.ts
+++ my-event-app/src/app/mocks/mock-locations.ts
@@ -1,17 +1,17 @@
 import { Location } from '../types/location';
 
 export const LOCATION: Location[] = [
-  { id: 1, name: 'Grazer Messe', description: 'sth about Grazer Messe', address: 'Messeplatz 1', zipCode: '8010', city: 'Graz', countryAbb: 'AUT'},
-  { id: 2, name: 'Wiener Stadthalle', description: 'sth about Wiener Stadthalle', address: 'Roland-Rainer-Platz 1', zipCode: '1150', city: 'Wien', countryAbb: 'AUT'},
-  { id: 3, name: 'Pannonia Fields', description: 'sth about Pannonia Fields', address: 'Grenzlandhof', zipCode: '2425', city: 'Nickelsdorf', countryAbb: 'AUT'},
-  { id: 4, name: 'Green Park', description: 'sth about Green Park', address: 'Kelsengasse 9', zipCode: '3100', city: 'St. Pölten', countryAbb: 'AUT'},
-  { id: 5, name: 'Reifnitz Freigelände', description: 'sth about Reifnitz', address: 'Wörthersee Süduferstraße 115', zipCode: '9081', city: 'Reifnitz', countryAbb: 'AUT' },
-  { id: 6, name: 'Schwarzlsee', description: 'sth. about Schwarzlsee', address: 'Thalerhofstraße 85', zipCode: '8141', city: 'Premstätten', countryAbb: 'AUT' },
-  { id: 7, name: 'Nürburgring', description: 'sth about Nürburgring', address: 'Otto-Flimm-Straße', zipCode: '53520', city: 'Eifel', countryAbb: 'BRD' },
-  { id: 8, name: 'Ernst Happel Stadion', description: 'sth about Wiener Ernst Happel Stadion', address: 'Meierstraße 7', zipCode: '1020', city: 'Wien', countryAbb: 'AUT'},
-  { id: 9, name: 'Messe Center Wien', description: 'sth about Wiener Messe', address: 'Messeplatz 1', zipCode: '1021', city: 'Wien', countryAbb: 'AUT'},
-  { id: 10, name: 'Royal Albert Hall', description: 'sth about Royal Albert Hall', address: 'Kensington Gore', zipCode: 'SW7 2AP', city: 'London', countryAbb: 'GBR'},
-  { id: 11, name: 'Wembley Stadion', description: 'sth about Wembley Stadion', address: 'Wembley', zipCode: 'HA9 0WS', city: 'London', countryAbb: 'GBR'}
+  { id: 1, name: 'Grazer Messe', description: 'Sth about Grazer Messe should be said in 3 rows at most. This is a short example what could have been.', address: 'Messeplatz 1', zipCode: '8010', city: 'Graz', countryAbb: 'AUT'},
+  { id: 2, name: 'Wiener Stadthalle', description: 'Sth about Wiener Stadthalle should be said in 3 rows at most. This is a short example what could have been.', address: 'Roland-Rainer-Platz 1', zipCode: '1150', city: 'Wien', countryAbb: 'AUT'},
+  { id: 3, name: 'Pannonia Fields', description: 'Sth about Pannonia Fields should be said in 3 rows at most. This is a short example what could have been.', address: 'Grenzlandhof', zipCode: '2425', city: 'Nickelsdorf', countryAbb: 'AUT'},
+  { id: 4, name: 'Green Park', description: 'Sth about Green Park should be said in 3 rows at most. This is a short example what could have been.', address: 'Kelsengasse 9', zipCode: '3100', city: 'St. Pölten', countryAbb: 'AUT'},
+  { id: 5, name: 'Reifnitz Freigelände', description: 'Sth about Reifnitz should be said in 3 rows at most. This is a short example what could have been.', address: 'Wörthersee Süduferstraße 115', zipCode: '9081', city: 'Reifnitz', countryAbb: 'AUT' },
+  { id: 6, name: 'Schwarzlsee', description: 'Sth about Schwarzlsee should be said in 3 rows at most. This is a short example what could have been.', address: 'Thalerhofstraße 85', zipCode: '8141', city: 'Premstätten', countryAbb: 'AUT' },
+  { id: 7, name: 'Nürburgring', description: 'Sth about Nürburgring should be said in 3 rows at most. This is a short example what could have been.', address: 'Otto-Flimm-Straße', zipCode: '53520', city: 'Eifel', countryAbb: 'BRD' },
+  { id: 8, name: 'Ernst Happel Stadion', description: 'Sth about Wiener Ernst Happel Stadion should be said in 3 rows at most. This is a short example what could have been.', address: 'Meierstraße 7', zipCode: '1020', city: 'Wien', countryAbb: 'AUT'},
+  { id: 9, name: 'Messe Center Wien', description: 'Sth about Messe Center Wien should be said in 3 rows at most. This is a short example what could have been.', address: 'Messeplatz 1', zipCode: '1021', city: 'Wien', countryAbb: 'AUT'},
+  { id: 10, name: 'Royal Albert Hall', description: 'Sth about Royal Albert Hall should be said in 3 rows at most. This is a short example what could have been.', address: 'Kensington Gore', zipCode: 'SW7 2AP', city: 'London', countryAbb: 'GBR'},
+  { id: 11, name: 'Wembley Stadion', description: 'Sth about Wembley Stadion should be said in 3 rows at most. This is a short example what could have been.', address: 'Wembley', zipCode: 'HA9 0WS', city: 'London', countryAbb: 'GBR'}
 
 
 
