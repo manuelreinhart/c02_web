7de785b2336a8e892a014dbe863621ac268330c8
GeSu07 <gerald.suessmaier@edu.campus02.at>
2019-01-23 21:39:17 +0100

    location mock angepasst

--- my-event-app/src/app/mocks/mock-locations.ts
+++ my-event-app/src/app/mocks/mock-locations.ts
@@ -7,7 +7,7 @@ export const LOCATION: Location[] = [
   { id: 4, name: 'Green Park', description: 'sth about Green Park', address: 'Kelsengasse 9', zipCode: '3100', city: 'St. Pölten', countryAbb: 'AUT'},
   { id: 5, name: 'Reifnitz Freigelände', description: 'sth about Reifnitz', address: 'Wörthersee Süduferstraße 115', zipCode: '9081', city: 'Reifnitz', countryAbb: 'AUT' },
   { id: 6, name: 'Schwarzlsee', description: 'sth. about Schwarzlsee', address: 'Thalerhofstraße 85', zipCode: '8141', city: 'Premstätten', countryAbb: 'AUT' },
-  { id: 7, name: 'Nürburgring/Eifel', description: 'sth about Nürburgring', address: 'Otto-Flimm-Straße', zipCode: '53520', city: 'Nürburgring/Eifel', countryAbb: 'BRD' },
+  { id: 7, name: 'Nürburgring', description: 'sth about Nürburgring', address: 'Otto-Flimm-Straße', zipCode: '53520', city: 'Eifel', countryAbb: 'BRD' },
   { id: 8, name: 'Ernst Happel Stadion', description: 'sth about Wiener Ernst Happel Stadion', address: 'Meierstraße 7', zipCode: '1020', city: 'Wien', countryAbb: 'AUT'},
   { id: 9, name: 'Messe Center Wien', description: 'sth about Wiener Messe', address: 'Messeplatz 1', zipCode: '1021', city: 'Wien', countryAbb: 'AUT'},
   { id: 10, name: 'Royal Albert Hall', description: 'sth about Royal Albert Hall', address: 'Kensington Gore', zipCode: 'SW7 2AP', city: 'London', countryAbb: 'GBR'},
